41a97927e7fe48ba6ee8517068edf04a77a01552
Francis Pion <francis.pion@akinox.com>
2022-04-06 12:37:12 -0400

    Implemented character management.

---
+++ api/src/SkillCraft.Core/Mapping/CharacterProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SkillCraft.Core.Characters;
+using SkillCraft.Core.Characters.Models;
+
+namespace SkillCraft.Core.Mapping
+{
+  internal class CharacterProfile : Profile
+  {
+    public CharacterProfile()
+    {
+      CreateMap<Character, CharacterModel>().MapAggregate();
+    }
+  }
+}
---
+++ api/src/SkillCraft.Infrastructure/Configurations/CharacterConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillCraft.Core.Characters;
+
+namespace SkillCraft.Infrastructure.Configurations
+{
+  internal class CharacterConfiguration : AggregateConfiguration, IEntityTypeConfiguration<Character>
+  {
+    public void Configure(EntityTypeBuilder<Character> builder)
+    {
+      base.Configure(builder);
+
+      builder.HasIndex(x => x.Name);
+
+      builder.Property(x => x.Age).HasDefaultValue(0);
+      builder.Property(x => x.BloodAlcoholContent).HasDefaultValue(0);
+      builder.Property(x => x.Experience).HasDefaultValue(0);
+      builder.Property(x => x.Intoxication).HasDefaultValue(0);
+      builder.Property(x => x.Name).HasMaxLength(100);
+      builder.Property(x => x.Player).HasMaxLength(100);
+      builder.Property(x => x.Stamina).HasDefaultValue(0);
+      builder.Property(x => x.Stature).HasDefaultValue(0.0);
+      builder.Property(x => x.Weight).HasDefaultValue(0.0);
+      builder.Property(x => x.Vitality).HasDefaultValue(0);
+    }
+  }
+}
